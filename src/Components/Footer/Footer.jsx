02f01ae25edbe82df166b157e99c355d30f70eef
Naman <109201101+naman29saini@users.noreply.github.com>
2024-10-28 15:23:43 +0530

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>CopyRight @ <span>Naman Saini</span> - All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
